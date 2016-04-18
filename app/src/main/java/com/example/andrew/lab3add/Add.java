74c9377e761f10824af94ccb6e912a2380aeb474
Andrew Yin <a5yin@ucsd.edu>
2016-04-18 00:27:34 -0700

    add functionality working

--- app/src/main/java/com/example/andrew/lab3add/Add.java
+++ app/src/main/java/com/example/andrew/lab3add/Add.java
@@ -9,8 +9,16 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Add extends AppCompatActivity {
+
+    Add AddActivity;
+
+    private Button addClick;
+    private EditText edit1, edit2;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,28 @@ public class Add extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        addClick = (Button) findViewById(R.id.addbutton);
+        edit1 = (EditText) findViewById(R.id.num1);
+        edit2 = (EditText) findViewById(R.id.num2);
+        text = (TextView) findViewById(R.id.sum);
+        addClick.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String str1, str2;
+                str1 = edit1.getText().toString();
+                str2 = edit2.getText().toString();
+                if( str1 == "" || str2 == "")
+                    text.setText("gg son");
+                else {
+                    int numb1 = Integer.parseInt(str1);
+                    int numb2 = Integer.parseInt(str2);
+                    int number = numb1+numb2;
+                    text.setText("" + number);
+                }
+
             }
         });
     }
